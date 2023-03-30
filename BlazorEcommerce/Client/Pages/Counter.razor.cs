266cf0f2345448b85c5233d17ac40d15b152c60d
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2023-03-30 12:24:27 +0330

    added Radzen.Blazor

---
+++ BlazorEcommerce/Client/Pages/Counter.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace BlazorEcommerce.Client.Pages;
+
+public partial class Counter
+{
+    [Inject] public NotificationService _notificationService {get ; set; } = null!;
+
+    private int currentCount = 0;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+
+        if (currentCount % 7 == 0)
+        {
+            _notificationService.Notify(
+                NotificationSeverity.Success,
+                "You are doing good",
+                "this is details",
+                5e3);
+        }
+    }
+}
