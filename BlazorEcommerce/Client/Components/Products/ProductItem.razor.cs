5e751324b83c8fb44d85808a7540986f4707f452
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2023-04-09 22:13:45 +0330

    ProductItem as Card and ProductList in Grid

---
+++ BlazorEcommerce/Client/Components/Products/ProductItem.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorEcommerce.Client.Components.Products;
+
+public partial class ProductItem
+{
+    [Parameter]
+    public Product Product { get; set; } = null!;
+}
---
+++ run.bat
@@ -0,0 +1,1 @@
+dotnet run --project .\BlazorEcommerce\Server\BlazorEcommerce.Server.csproj
